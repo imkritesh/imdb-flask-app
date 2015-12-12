01acd567d45ded5cf94d5f9cf88554e565b6d0aa
imkritesh <kritesh94semwal.ks@gmail.com>
2015-12-13 05:05:30 +0530

    Scraping movie details by movie id

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, redirect, url_for
 from imdb import *
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
-def showHomePage():
+def homePage():
 	actors = []	
 	query = request.args.get('query')
 	if query is not None and query:
 		actors = find_by_name(query)
 		#print str(actors)
 	return render_template('home.html', actors = actors, query = query)
-
+@app.route('/person/')
+@app.route('/person/<actor_id>')
+def resultPage(actor_id = None):
+	if actor_id:
+		return actor_id
+	else:
+		return redirect(url_for('homePage'))
 if __name__=='__main__':
 	app.run(threaded=True,debug=True)
--- imdb.py
+++ imdb.py
@@ -32,5 +32,50 @@ def find_by_name(name):
 			col["bio"] = bio.text
 		query_result.append(col)
 	return query_result
+
+def get_movie_detail_by_movie_id(movie_id):
+	r = req.get('http://www.imdb.com/title/'+movie_id)
+	soup = bs(r.text)
+	display = soup.find('div',{'class':'poster'})
+	display_pic = display.find('a').find('img')['src']
+	infobar = soup.find('div',{'class':'subtext'})
+	title = soup.find('title').string
+	title = title.split('-')[0].strip()
+	length = infobar.find('time').string.strip()
+	genre_list = infobar.findAll('a')
+	for i in range(len(genre_list)-1):
+		genre_list[i] = genre_list[i].string
+	genre = ','.join(genre_list[i] for i in range(len(genre_list)-1))
+	release_date = genre_list[-1].text.strip()
+	rate = soup.find('span',itemprop='ratingValue').string
+	count = soup.find('span',itemprop='ratingCount').string
+	rating = rate + '/10 from ' + count + ' users'
+	return display_pic, title, length, genre, release_date
+
+def get_reviews_by_movie_id(movie_id):
+	'''
+	Returns top 3 reviews of the movie
+	'''
+
+def find_movies_by_actor_id(actor_id):
+	'''
+	Return top 3 movies by an actor 
+	using actor_id
+	'''
+	r = req.get('http://www.imdb.com/name/'+actor_id)
+	soup = bs(r.text)
+	movie_div = soup.find("div", {"id":"knownfor"})
+	divs = movie_div.findAll("div")
+	divs = divs[:3]#To get only Top 3 movies.
+	movie_list = []
+	for div in divs:
+		movie = {}
+		url = div.find("a")["href"]
+		url = url.split('/')
+		movie["movie_id"] = url[2]
+		movie["display_pic"],movie["title"],movie["length"],movie["genre"],movie["release_date"] = get_movie_detail_by_movie_id(movie["movie_id"])
+		movie_list.append(movie)
+	print movie_list
 		
 #find_by_name('shah')
+#find_movies_by_actor_id('nm1229940')

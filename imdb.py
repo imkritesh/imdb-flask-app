6199d60c2cea43976e2fd2f0ce9be2ac29807437
imkritesh <kritesh94semwal.ks@gmail.com>
2015-12-13 07:11:12 +0530

    Scraping reviews by movie id added

--- imdb.py
+++ imdb.py
@@ -1,5 +1,6 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
+import re
 
 def find_by_name(name):
 	'''
@@ -52,10 +53,32 @@ def get_movie_detail_by_movie_id(movie_id):
 	rating = rate + '/10 from ' + count + ' users'
 	return display_pic, title, length, genre, release_date
 
-def get_reviews_by_movie_id(movie_id):
+def get_reviews_by_movie_id(movie_id, count=3):
 	'''
 	Returns top 3 reviews of the movie
 	'''
+	extra_string = '*** This review may contain spoilers ***'
+	r = req.get('http://www.imdb.com/title/'+movie_id+'/reviews')
+	soup = bs(r.text)
+	text_list = soup.findAll('p',{'class':''})[5:]
+	rev = []
+	for i in range(count):
+		if text_list[i].string == extra_string:
+			i-=1
+		rev.append(text_list[i])
+	text_list = rev
+	headings =[heading.string for heading in soup.findAll('h2')[:3]]
+	authors = soup.findAll('a', {'href':re.compile('user')})[1:6:2]
+	authors = [author.string for author in authors]
+	reviews = []
+	for i in range(count):
+		review = {}
+		review['text'] = text_list[i]
+		review['heading'] = headings[i]
+		review['author'] = authors[i]
+		reviews.append(review)
+	print reviews[0]
+	return reviews
 
 def find_movies_by_actor_id(actor_id):
 	'''
@@ -79,3 +102,4 @@ def find_movies_by_actor_id(actor_id):
 		
 #find_by_name('shah')
 #find_movies_by_actor_id('nm1229940')
+get_reviews_by_movie_id('tt2224317')
